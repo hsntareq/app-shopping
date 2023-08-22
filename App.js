2aed6fe1208f56384fc6a0d01d41b9cef7860815
SaBiT495 <shahedbillah495@gmail.com>
2023-08-22 17:00:55 +0600

    category input done

--- App.js
+++ App.js
@@ -5,6 +5,13 @@ import { StyleSheet, Button, Text, TextInput, View, TouchableHighlight, Alert } 
 export default function App() {
   const [title, setTitle] = useState('');
   const [price, setPrice] = useState('');
+  const [cates, setCates] = useState([
+    { id: 1, title: "Mom" },
+    { id: 2, title: "Personal" },
+    { id: 3, title: "Org" },
+    { id: 4, title: "Office" }
+  ]);
+  // console.log(cates);
 
   const handleAddItem = () => {
     console.log('Title:', title);
@@ -14,11 +21,13 @@ export default function App() {
     Alert.alert('You tapped the button!');
   }
 
+
+
   return (
     <View className="flex-1 bg-cyan-800">
-      <Text className="bg-green-500 p-5 w-full  text-center font-bold text-3xl">Shopping List</Text>
-      <View className="w-full bg-yellow-500 flex-1 overflow-y-scroll flex flex-col px-8 py-5">
-        <View className="flex flex-row">
+      <Text className="bg-lime-500 p-5 w-full  text-center font-bold text-3xl">Shopping List</Text>
+      <View className="w-full bg-lime-400 flex-1 overflow-y-scroll flex flex-col px-8 py-5">
+        {/*  <View className="flex flex-row">
           <TextInput
             className="flex-1 border px-4 focus:bg-lime-200 rounded-none"
             placeholder="Title"
@@ -33,17 +42,36 @@ export default function App() {
             keyboardType="numeric"
           />
           <Button className="px-3 rounded-none" title="+ Add" onPress={handleAddItem} />
+        </View> */}
+
+        <View className="flex flex-row">
+          <TextInput
+            className="flex-1 border px-4 focus:bg-lime-200 border-lime-700 rounded-none "
+            placeholder="Type a Category"
+            value={title}
+            onChangeText={text => setTitle(text)}
+          />
+         
+          <Button className="px-3 rounded-none" title="+ Add" onPress={handleAddItem} />
         </View>
         <View className="flex-1 justify-center items-center">
-          <View >
-            <TouchableHighlight onPress={_onPressButton} underlayColor="white">
-              <View >
-                <Text >TouchableHighlight</Text>
-              </View>
-            </TouchableHighlight>
+          <View>
+            {cates.map((category) => (
+              <TouchableHighlight
+                style="border px-4"
+                key={category.id}
+                onPress={_onPressButton}
+                underlayColor="white"
+              >
+                <View >
+                  <Text className="border  mb-2 px-4 font-bold border-lime-600 bg-lime-500">{category.title}</Text>
+                </View>
+              </TouchableHighlight>
+            ))}
           </View>
+
           <View className="px-3 rounded-none absolute right-0 bottom-0">
-            <Button title="+ Add" onPress={handleAddItem} />
+            <Button  title="+ Add" onPress={handleAddItem} />
           </View>
         </View>
       </View>
